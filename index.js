de5162c2187dc63d3a2b017d30022bbd8559d8c5
betterside <bettersideserver@gmail.com>
2026-01-05 16:43:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Aman 🚀 Railway is working");
+app.get("/about", (req, res) => {
+  res.send("This is Aman’s first Railway backend");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
